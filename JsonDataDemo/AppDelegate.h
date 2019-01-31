df96d72ac291cb871284e3221154812f6dc3c1b4
wangxu <xwangx@ctrip.com>
2019-01-31 15:04:27 +0800

    Initial Commit

---
+++ JsonDataDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JsonDataDemo
+//
+//  Created by nate on 2019/1/31.
+//  Copyright © 2019 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JsonDataDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JsonDataDemo
+//
+//  Created by nate on 2019/1/31.
+//  Copyright © 2019 nate. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
